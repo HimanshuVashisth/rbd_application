d171d87cff24c10294ebc27c01d7db04fea05907
LAPTOP-B62P5VM9\Harshita <shrma.varsha22@gmail.com>
2022-02-12 12:33:41 +0530

    merged

--- config.js
+++ config.js
@@ -1,23 +1,19 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({
-    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
-});
-
-async function getConfig(ip, dbPort, uname, pwd, dbName) {
-    const config = {
-        db: {
-            host: ip,
-            port: dbPort,
-            user: uname,
-            password: pwd,
-            database: dbName,
-            waitForConnections: true,
-            connectionLimit: 10,
-            queueLimit: 0
-        }
-    }
+const config = {
+    db: { /* don't expose password or any sensitive info, done only for demo */
+        host: env.DB_HOST || '172.17.0.1',
+        port: env.DB_PORT || '3306',
+        user: env.DB_USER || 'root',
+        password: env.DB_PASSWORD || 'root1234',
+        database: env.DB_NAME || 'classicModels',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    },
+    listPerPage: env.LIST_PER_PAGE || 10,
+};
 
     return config;
 }
